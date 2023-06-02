e1e8b7689fb38ca2c7c015cdec0f247000e41adc
mfareed1947 <fareed1504@gmail.com>
2023-06-03 04:13:24 +0500

    create a hero section comp

---
+++ Components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='flex'>
+            {/* image right side div */}
+            <div></div>
+            {/* image right side div */}
+            <div></div>
+        </div>
+    )
+}
+
+export default Hero
